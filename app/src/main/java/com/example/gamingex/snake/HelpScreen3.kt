51913ddba98eb01b8d8204e17c350b34586fd11b
gazinaft <gazinaft@gmail.com>
2020-05-23 11:31:38 +0300

    Further development of a snake game

---
+++ app/src/main/java/com/example/gamingex/snake/HelpScreen3.kt
@@ -0,0 +1,34 @@
+package com.example.gamingex.snake
+
+import com.example.gamingex.framework.Game
+import com.example.gamingex.framework.Input
+import com.example.gamingex.framework.Screen
+
+class HelpScreen3(game: Game): Screen(game) {
+
+    override fun update(deltaTime: Float) {
+        val touchEvents = game.getInput().touchEvents
+        game.getInput().keyEvents
+        for (event in touchEvents) {
+            if (event.type == Input.Companion.TouchEvent.TOUCH_UP) {
+                if (event.x > 256 && event.y > 416) {
+                    game.setScreen(MainMenuScreen(game))
+                    if (Settings.soundEnabled) Assets.click.play(1f)
+                }
+            }
+        }
+    }
+
+
+    override fun present(deltaTime: Float) {
+        val g = game.getGraphics()
+        g.drawPixmap(Assets.backGround, 0, 0)
+        g.drawPixmap(Assets.help3, 64, 100)
+        g.drawPixmap(Assets.buttons, 256, 416, 0, 64, 64, 64)
+    }
+    override fun resume() = Unit
+
+    override fun pause() = Unit
+
+    override fun dispose() = Unit
+}
