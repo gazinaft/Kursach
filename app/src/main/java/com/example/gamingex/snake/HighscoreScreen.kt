652826167a4ff495b742e8822a6305c3203bcd67
gazinaft <gazinaft@gmail.com>
2020-05-23 11:31:38 +0300

    Further development of a snake game

---
+++ app/src/main/java/com/example/gamingex/snake/HighscoreScreen.kt
@@ -0,0 +1,72 @@
+package com.example.gamingex.snake
+
+import com.example.gamingex.framework.Game
+import com.example.gamingex.framework.Graphics
+import com.example.gamingex.framework.Input
+import com.example.gamingex.framework.Screen
+
+class HighscoreScreen(game: Game): Screen(game) {
+
+    val scores = List<String>(5) { "${(it+1)}. ${Settings.highscores[it]}" }
+
+    override fun update(deltaTime: Float) {
+        val touchEvents = game.getInput().touchEvents
+
+        //clear the buffer of keyEvents
+        game.getInput().keyEvents
+
+        for (event in touchEvents) {
+            if (event.type == Input.Companion.TouchEvent.TOUCH_UP &&
+                    event.x < 64 && event.y > 416) {
+                Assets.click.play(1f)
+                game.setScreen(MainMenuScreen(game))
+            }
+        }
+    }
+
+
+    override fun present(deltaTime: Float) {
+        val g = game.getGraphics()
+        g.drawPixmap(Assets.backGround, 0, 0)
+        g.drawPixmap(Assets.mainMenu, 64, 20, 0, 42, 196, 42)
+
+        var y = 100
+        for (i in scores) {
+            drawNumbers(g, i, 20, y)
+            y += 50
+        }
+
+        g.drawPixmap(Assets.buttons, 0, 416)
+
+    }
+
+    fun drawNumbers(g: Graphics, line: String, x: Int, y: Int) {
+        var xI = x
+        for (i in line.indices) {
+            val char = line[i]
+            if (char == ' ') {
+                xI += 20
+                continue
+            }
+            var srcWidth = 0
+            var srcX = 0
+
+            if (char == '.') {
+                srcWidth = 10
+                srcX = 200
+            } else {
+                srcX = (char - '0') * 20
+                srcWidth = 20
+            }
+            g.drawPixmap(Assets.numbers, xI, y, srcX, 0, srcWidth, 32 )
+            xI += srcWidth
+        }
+    }
+
+    override fun pause() = Unit
+
+    override fun resume() = Unit
+
+    override fun dispose() = Unit
+
+}
