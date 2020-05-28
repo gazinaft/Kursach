2b257d390ea4f69b623dd20e2c8993104720e4b2
gazinaft <gazinaft@gmail.com>
2020-05-28 11:38:36 +0300

    Rework of Helpscreens
    This rework is made to remove the repeating code and rework the helpscreens

--- app/src/main/java/com/example/gamingex/snake/HelpScreen.kt
+++ app/src/main/java/com/example/gamingex/snake/HelpScreen.kt
@@ -5,32 +5,7 @@ import com.example.gamingex.framework.Input
 import com.example.gamingex.framework.Screen
 
 
-//this is first of 3 helpscreens which will help to deal with gameplay
-class HelpScreen(game: Game): Screen(game) {
-
-    override fun update(deltaTime: Float) {
-        val touchEvents = game.getInput().touchEvents
-        game.getInput().keyEvents
-        for (event in touchEvents) {
-            if (event.type == Input.Companion.TouchEvent.TOUCH_UP) {
-                if (event.x > 256 && event.y > 416) {
-                    game.setScreen(HelpScreen2(game))
-                    if (Settings.soundEnabled) Assets.click.play(1f)
-                }
-            }
-        }
-    }
-
-
-    override fun present(deltaTime: Float) {
-        val g = game.getGraphics()
-        g.drawPixmap(Assets.backGround, 0, 0)
-        g.drawPixmap(Assets.help1, 64, 100)
-        g.drawPixmap(Assets.buttons, 256, 416, 0, 64, 64, 64)
-    }
-    override fun resume() = Unit
-
-    override fun pause() = Unit
-
-    override fun dispose() = Unit
+//this is first of 3 helpScreens which will help to deal with gameplay
+class HelpScreen(game: Game): HelpScreenAbstract(game) {
+    override val order: Int = 1
 }
--- app/src/main/java/com/example/gamingex/snake/HelpScreen2.kt
+++ app/src/main/java/com/example/gamingex/snake/HelpScreen2.kt
@@ -4,31 +4,6 @@ import com.example.gamingex.framework.Game
 import com.example.gamingex.framework.Input
 import com.example.gamingex.framework.Screen
 
-class HelpScreen2(game: Game): Screen(game) {
-
-    override fun update(deltaTime: Float) {
-        val touchEvents = game.getInput().touchEvents
-        game.getInput().keyEvents
-        for (event in touchEvents) {
-            if (event.type == Input.Companion.TouchEvent.TOUCH_UP) {
-                if (event.x > 256 && event.y > 416) {
-                    game.setScreen(HelpScreen3(game))
-                    if (Settings.soundEnabled) Assets.click.play(1f)
-                }
-            }
-        }
-    }
-
-
-    override fun present(deltaTime: Float) {
-        val g = game.getGraphics()
-        g.drawPixmap(Assets.backGround, 0, 0)
-        g.drawPixmap(Assets.help2, 64, 100)
-        g.drawPixmap(Assets.buttons, 256, 416, 0, 64, 64, 64)
-    }
-    override fun resume() = Unit
-
-    override fun pause() = Unit
-
-    override fun dispose() = Unit
+class HelpScreen2(game: Game): HelpScreenAbstract(game) {
+    override val order: Int = 2
 }
