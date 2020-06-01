81dcd3db023f2d910ea4190f7fdb51ac94f3ff4c
gazinaft <gazinaft@gmail.com>
2020-06-01 18:09:39 +0300

    Foundation of main tetris logic

--- app/src/main/java/com/example/gamingex/tetris/Block.kt
+++ app/src/main/java/com/example/gamingex/tetris/Block.kt
@@ -1,6 +1,7 @@
 package com.example.gamingex.tetris
 
-class Block(x: Int, y: Int, shape: Int ) {
+class Block(var x: Int, var y: Int, val shape: Int ) {
+
     companion object {
         //00
         //00
@@ -18,12 +19,47 @@ class Block(x: Int, y: Int, shape: Int ) {
         const val ZIGZAG2 = 3
         //0
         //000
-        const val CURVY = 4
+        const val CURVY1 = 4
+        //  0
+        //000
+        const val CURVY2 = 5
         // 0
         //000
-        const val LADDER = 5
+        const val LADDER = 6
+    }
+    //structure of a particular block
+    // which is responsible for the order and place of the block in a game
+    //depends on a shape of each block
+    //the x and y are the coordinates of lower right brick
+    val structure = when(shape){
+        SQUARE ->
+            arrayListOf<Brick>(Brick(x - 1, y - 1), Brick(x, y - 1), Brick(x - 1, y))
+        LINE ->
+            arrayListOf<Brick>(Brick(x, y - 3), Brick(x, y - 2), Brick(x, y - 1))
+        ZIGZAG1 ->
+            arrayListOf<Brick>(Brick(x, y - 1), Brick(x + 1, y - 1), Brick(x - 1, y))
+        ZIGZAG2 ->
+            arrayListOf<Brick>(Brick(x - 2, y - 1), Brick(x - 1, y - 1), Brick(x - 1, y))
+        CURVY1 ->
+            arrayListOf<Brick>(Brick(x - 2, y - 1), Brick(x - 2, y), Brick(x - 1, y))
+        CURVY2 ->
+            arrayListOf<Brick>(Brick(x, y - 1), Brick(x - 2, y), Brick(x - 1, y))
+        LADDER ->
+            arrayListOf<Brick>(Brick(x - 1, y - 1), Brick(x - 2, y), Brick(x - 1, y))
+        else ->
+            throw IllegalStateException("There are only 7 shapes of blocks in Tetris")
+    }.apply { add(Brick(x, y)) }
+
+    fun rotated(): Collection<Brick> = when (shape) {
+        SQUARE ->
+            structure.toMutableList()
+
+        else ->
+            throw IllegalStateException("There are only 7 shapes of blocks in Tetris")
     }
 
-
+    fun moveDown() {
+        structure.forEach { it.y += 1 }
+    }
 
 }
---
+++ app/src/main/java/com/example/gamingex/tetris/Brick.kt
@@ -0,0 +1,3 @@
+package com.example.gamingex.tetris
+
+class Brick(var x: Int, var y: Int)
